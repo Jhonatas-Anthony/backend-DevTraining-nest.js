075754a1915c9a4762c09c3da081066f4d93d322
Jhonatas Anthony <jhonatasanthony6@gmail.com>
2024-06-06 15:41:33 -0300

    custom response status code

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,22 +1,23 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Res } from '@nestjs/common';
 
 @Controller('courses')
 export class CoursesController {
   @Get()
-  findAll() {
-    return 'Listagem de cursos';
+  findAll(@Res() response) {
+    //return response.status(200).send('Listagem de cursos');
+    return response.status(200).json({ message: 'Listagem de cursos' });
   }
 
   @Get('find/:id')
+  // Com desconstrução: findOne(@Param('id') id: string)
+  // Sem: findOne(@Param() params) -> params.id
   findOne(@Param('id') id: string) {
     return 'Return obj ' + id;
   }
 
+  // @HttpCode(204) Usado para excluir recursos
   @Post()
   create(@Body() body) {
     return body;
   }
-
-  // Com desconstrução: findOne(@Param('id') id: string)
-  // Sem: findOne(@Param() params) -> params.id
 }
