be219aa7029f2848bf01873b3d7aeca46c60132f
Jhonatas Anthony <jhonatasanthony6@gmail.com>
2024-06-08 11:46:36 -0300

    Creating entity and implements array of courses on service file

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { Course } from './courses.entity';
 
 @Injectable()
-export class CoursesService {}
+export class CoursesService {
+  private courses: Course[] = [
+    {
+      id: 1,
+      name: 'Nestjs',
+      description: 'Curso sobre fundamentos do nestjs',
+      tags: ['node.js', 'nest.js', 'javascript', 'typescript'],
+    },
+  ];
+}
