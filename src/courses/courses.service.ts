44984fc4f1018e6a8e8da8c8bc1b08c1ed8c6e9a
Jhonatas Anthony <jhonatasanthony6@gmail.com>
2024-06-08 12:38:08 -0300

    create crud operations on courses services

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -11,4 +11,36 @@ export class CoursesService {
       tags: ['node.js', 'nest.js', 'javascript', 'typescript'],
     },
   ];
+
+  findAll() {
+    return this.courses;
+  }
+
+  find(id: number) {
+    return this.courses.find((course) => course.id === id);
+  }
+
+  // TODO Mudar para DTO depois
+  create(data: any) {
+    this.courses.push(...data);
+  }
+
+  // TODO Mudar para DTO depois
+  update(id: number, data: any) {
+    const existingCourse = this.find(id);
+    if (existingCourse) {
+      const index = this.courses.findIndex((course) => course.id === id);
+      this.courses[index] = {
+        id,
+        ...data,
+      };
+    }
+  }
+
+  remove(id: number) {
+    const index = this.courses.findIndex((course) => course.id === id);
+    if (index >= 0) {
+      this.courses.splice(index, 1);
+    }
+  }
 }
