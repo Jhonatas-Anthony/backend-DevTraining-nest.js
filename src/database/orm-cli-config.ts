cba9401d48e9c4004b9d3a3a7b77af0d9faa8bb8
Jhonatas Anthony <jhonatasanthony6@gmail.com>
2024-06-12 10:46:00 -0300

    Create Migration for Tags Table

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -1,9 +1,10 @@
 import { DataSource } from 'typeorm';
 import { dataSourceOptions } from './database.module';
 import { CreateCoursesTable1718197172385 } from 'src/migrations/1718197172385-CreateCoursesTable';
+import { CreateTagsTable1718199702417 } from 'src/migrations/1718199702417-CreateTagsTable';
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
   synchronize: false,
-  migrations: [CreateCoursesTable1718197172385],
+  migrations: [CreateCoursesTable1718197172385, CreateTagsTable1718199702417],
 });
