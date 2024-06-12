e4e381f8523a50a82fa7b2477c1417ddb776ac46
Jhonatas Anthony <jhonatasanthony6@gmail.com>
2024-06-12 13:01:28 -0300

    Create Migration to Many yo Many Relation between courses and tags

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -2,9 +2,14 @@ import { DataSource } from 'typeorm';
 import { dataSourceOptions } from './database.module';
 import { CreateCoursesTable1718197172385 } from 'src/migrations/1718197172385-CreateCoursesTable';
 import { CreateTagsTable1718199702417 } from 'src/migrations/1718199702417-CreateTagsTable';
+import { CreateCoursesTagsTable1718207634898 } from 'src/migrations/1718207634898-CreateCoursesTagsTable';
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
   synchronize: false,
-  migrations: [CreateCoursesTable1718197172385, CreateTagsTable1718199702417],
+  migrations: [
+    CreateCoursesTable1718197172385,
+    CreateTagsTable1718199702417,
+    CreateCoursesTagsTable1718207634898,
+  ],
 });
