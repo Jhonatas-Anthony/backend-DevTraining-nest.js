0434832edd7e0adf8cedfebfc8b474bffae0143b
Jhonatas Anthony <jhonatasanthony6@gmail.com>
2024-06-12 13:14:43 -0300

    Adding foreign key to MM table courses_tags

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -3,6 +3,7 @@ import { dataSourceOptions } from './database.module';
 import { CreateCoursesTable1718197172385 } from 'src/migrations/1718197172385-CreateCoursesTable';
 import { CreateTagsTable1718199702417 } from 'src/migrations/1718199702417-CreateTagsTable';
 import { CreateCoursesTagsTable1718207634898 } from 'src/migrations/1718207634898-CreateCoursesTagsTable';
+import { AddCoursesIdToCoursesTagsTable1718208150313 } from 'src/migrations/1718208150313-AddCoursesIdToCoursesTagsTable';
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
@@ -11,5 +12,6 @@ export const dataSource = new DataSource({
     CreateCoursesTable1718197172385,
     CreateTagsTable1718199702417,
     CreateCoursesTagsTable1718207634898,
+    AddCoursesIdToCoursesTagsTable1718208150313,
   ],
 });
