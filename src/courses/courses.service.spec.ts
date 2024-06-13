12ecbb7c021572e2c905e61eec5aba63d95b0f53
Jhonatas Anthony <jhonatasanthony6@gmail.com>
2024-06-13 09:12:37 -0300

    Start new unit test for Courses Service

--- src/courses/courses.service.spec.ts
+++ src/courses/courses.service.spec.ts
@@ -1,15 +1,45 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { randomUUID } from 'node:crypto';
 import { CoursesService } from './courses.service';
 
-describe('CoursesService', () => {
+describe('CoursesService unit tests', () => {
   let service: CoursesService;
+  let id: string;
+  let created_at: Date;
+  let expectedOutputTags: any[];
+  let expectOutputCourses: any;
+
+  let mockCoursesRepository: any;
+  let mockTagRepository: any;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [CoursesService],
-    }).compile();
+    service = new CoursesService();
+    id = randomUUID();
+    created_at = new Date();
 
-    service = module.get<CoursesService>(CoursesService);
+    expectedOutputTags = [{ id: id, name: 'nestjs', created_at: created_at }];
+    expectOutputCourses = {
+      id: id,
+      name: 'test',
+      descrption: 'test course',
+      created_at: created_at,
+      tags: expectedOutputTags,
+    };
+
+    mockCoursesRepository = {
+      create: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+      save: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+      update: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+      preload: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+      findAll: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+      findOne: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+      find: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+      remove: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+    };
+
+    mockTagRepository = {
+      create: jest.fn().mockReturnValue(Promise.resolve(expectedOutputTags)),
+      findAll: jest.fn().mockReturnValue(Promise.resolve(expectedOutputTags)),
+    };
   });
 
   it('should be defined', () => {
