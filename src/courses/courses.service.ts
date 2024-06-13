9fab333ffcfb18822566b2a3856f5abb0b5224fb
Jhonatas Anthony <jhonatasanthony6@gmail.com>
2024-06-13 09:12:13 -0300

    Removing constructor method from Courses service

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -8,13 +8,13 @@ import { UpdateCourseDTO } from './dto/update-course.dto';
 
 @Injectable()
 export class CoursesService {
-  constructor(
-    @InjectRepository(Course)
-    private readonly courseRepository: Repository<Course>,
+  /*   constructor( */
+  @InjectRepository(Course)
+  private readonly courseRepository: Repository<Course>;
 
-    @InjectRepository(Tag)
-    private readonly tagRepository: Repository<Tag>,
-  ) {}
+  @InjectRepository(Tag)
+  private readonly tagRepository: Repository<Tag>;
+  /*   ) {} */
 
   async findAll() {
     return await this.courseRepository.find({
